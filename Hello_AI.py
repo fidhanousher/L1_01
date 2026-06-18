905644e9a2d21e9d303fa9f3058947ebf19d4be2
fidhanousher <fidhanousher@gmail.com>
2026-06-18 21:57:23 +0530

    2nd commit

--- Hello_AI.py
+++ Hello_AI.py
@@ -1,36 +1,33 @@
-def mood_bot():
-    print("🤖 AI Bot: Hi! I am your mood assistant 😊")
 
-    mood = input("How are you feeling today? (good/bad): ").lower()
+print("🤖 AI Bot: Hi! I am your mood assistant 😊")
 
-    if mood == "good":
-        print("🤖 AI Bot: That's amazing! 🌟")
-        print("Keep smiling and enjoy your day!")
+mood = input("How are you feeling today? (good/bad): ").lower()
 
-        activity = input("Want a fun suggestion? (yes/no): ").lower()
+if mood == "good":
+    print("🤖 AI Bot: That's amazing! 🌟")
+    print("Keep smiling and enjoy your day!")
 
-        if activity == "yes":
+    activity = input("Want a fun suggestion? (yes/no): ").lower()
+
+    if activity == "yes":
             print("🤖 AI Bot: Try listening to music, going for a walk, or learning something new 🎵")
 
-        else:
+    else:
             print("🤖 AI Bot: No problem! Have a wonderful day 😊")
 
 
-    elif mood == "bad":
-        print("🤖 AI Bot: I'm sorry you are feeling that way 💙")
-        print("Remember, difficult moments don't last forever.")
+elif mood == "bad":
+    print("🤖 AI Bot: I'm sorry you are feeling that way 💙")
+    print("Remember, difficult moments don't last forever.")
 
-        choice = input("Would you like a motivation message? (yes/no): ").lower()
+    choice = input("Would you like a motivation message? (yes/no): ").lower()
 
-        if choice == "yes":
-            print("🤖 AI Bot: You are stronger than your problems. Take things one step at a time 🌱")
-
-        else:
-            print("🤖 AI Bot: That's okay. I hope you feel better soon ❤️")
-
+    if choice == "yes":
+        print("🤖 AI Bot: You are stronger than your problems. Take things one step at a time 🌱")
 
     else:
-        print("🤖 AI Bot: I didn't understand. Please type only good or bad.")
+        print("🤖 AI Bot: That's okay. I hope you feel better soon ❤️")
 
 
-mood_bot()
+else:
+    print("🤖 AI Bot: I didn't understand. Please type only good or bad.")
